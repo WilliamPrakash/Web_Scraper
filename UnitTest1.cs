2f0697be0f3d697ce02ac3cbc6a345350563c624
WilliamPrakash <willprakash@gmail.com>
2025-05-29 15:05:20 -0400

    Following YouTuber Execute Automation's selenium C# series

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,6 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace Web_Scraper;
 
 public class Tests
@@ -8,8 +11,23 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void Driver_Test()
     {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.google.com");
+        //driver.Manage().Window.Maximize();
+        IWebElement searchField = driver.FindElement(By.Id("input"));
+        searchField.SendKeys("YouTube");
+        searchField.SendKeys(Keys.Return);
+        //searchField.Click();
+
+        Console.WriteLine();
         Assert.Pass();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Mr. C# function, tear down ???
     }
 }
